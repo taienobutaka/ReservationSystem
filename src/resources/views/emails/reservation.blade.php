aa7e86aa449cd9b92e39e08c075a819c15707066
taie <taie0424engineer@gmail.com>
2025-02-06 12:30:40 +0900

    マイページ修正

--- src/resources/views/emails/reservation.blade.php
+++ src/resources/views/emails/reservation.blade.php
@@ -8,5 +8,6 @@
     <p>Thank you for your reservation.</p>
     <p>Reservation Date: {{ $reservation->start_at }}</p>
     <p>Number of Users: {{ $reservation->num_of_users }}</p>
+    <img src="data:image/png;base64,{{ $qrCode }}" alt="QR Code">
 </body>
 </html>
